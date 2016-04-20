1ebbdef876f8196468b8d6057e008d69373b05c1
sasviben <sandro.sviben@hotmail.com>
2016-04-20 21:18:25 +0200

    Dodana klasa PotapanjeBrodova

---
+++ PotapanjeBrodova/Topništvo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PotapanjeBrodova
+{
+    public class Topništvo
+    {
+        public RezultatGađanja UputiPucanj(Flota flota)
+        {
+
+            throw new NotImplementedException();
+        }
+
+        public void ObradiRezultatGađanja(RezultatGađanja rezultat)
+        {
+
+        }
+
+    }
+}
