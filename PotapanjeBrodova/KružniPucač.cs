af880608f546e4abaab78edb6101cfc4aa0015d6
sasviben <sandro.sviben@hotmail.com>
2016-05-11 20:49:12 +0200

    KružniPucač solved

--- PotapanjeBrodova/KružniPucač.cs
+++ PotapanjeBrodova/KružniPucač.cs
@@ -15,10 +15,31 @@ namespace PotapanjeBrodova
 
         public Polje UputiPucanj()
         {
-            throw new NotImplementedException();
+            int redak = prvoPogođeno.Redak;
+            int stupac = prvoPogođeno.Stupac;
+            //tražimo za sve smjerove broj slobodnih polja u tom smjeru 
+            List<IEnumerable<Polje>> kandidati = new List<IEnumerable<Polje>>();
+            foreach (Smjer smjer in Enum.GetValues(typeof(Smjer)))
+            {
+                kandidati.Add(mreža.DajPoljaUZadanomSmjeru(redak, stupac, smjer));
+            }
+
+            //sortiram te liste tako da je najdulja prva
+            kandidati.Sort((lista1, lista2) => lista2.Count() - lista1.Count());
+            //između najduljih biram jednu slučajnim odabirom 
+            var grupe = kandidati.GroupBy(lista => lista.Count());
+            var najdulji = grupe.First();
+            if (najdulji.Count() == 1)
+                return najdulji.First().First();
+            int indeks = slučajni.Next(najdulji.Count());
+            
+            //iz nje vadim prvi polje
+           return najdulji.ElementAt(indeks).First();
+
         }
 
         Polje prvoPogođeno;
         Mreža mreža;
+        Random slučajni = new Random();
     }
 }
