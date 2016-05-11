647ed07b979ee272d00337d911f500f4ec84fb06
sasviben <sandro.sviben@hotmail.com>
2016-05-11 19:09:40 +0200

    sa

---
+++ UnitTests/TestNapipača.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PotapanjeBrodova;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class TestNapipača
+    {
+        [TestMethod]
+        public void Napipač_ListaPoljaZaHorizontalniBrodDuljine3MoraBitiSadržavati15Polja()
+        {
+            Mreža m = new Mreža(1,7);
+            const int duljinaBroda = 3;
+            Napipač n = new Napipač(m, duljinaBroda);
+            Assert.AreEqual(15, n.DajKandidateZaHorizontalniBrod().Count());
+
+        }
+
+       /*  [TestMethod]
+        public void Napipač_ListaPoljaZaHorizontalniBrodDuljine3MoraBitiSadržavatiPoljaUOdređenomBroju()
+        {
+            Mreža m = new Mreža(1, 7);
+            const int duljinaBroda = 3;
+            Napipač n = new Napipač(m, duljinaBroda);
+            Assert.AreEqual(15, n.DajKandidateZaHorizontalniBrod().Count());
+
+        }*/
+        
+
+
+
+        [TestMethod]
+        public void Napipač_ListaPoljaZaVertikalniBrodDuljine4MoraBitiSadržavati16Polja()
+        {
+            Mreža m = new Mreža(5, 2);
+            const int duljinaBroda = 4;
+            Napipač n = new Napipač(m, duljinaBroda);
+            Assert.AreEqual(16, n.DajKandidateZaVertikalniBrod().Count());
+
+        }
+    }
+}
